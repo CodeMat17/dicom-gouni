899383bf9e70b30a99edb5d0c851b4a814540a4e
Chukwu Matthew <codemat.biz@gmail.com>
2024-08-24 15:36:33 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen flex flex-col items-center justify-center px-4 '>
+      <h1 className='text-4xl font-semibold text-center text-gray-500'>
+        This site is still being developed. Check again.
+      </h1>
+      <p className='mt-5'>DICOM GOUNI Management.</p>
+    </div>
+  );
+}
+
+export default Home
