6a18b2e40e2d496ef676a923a06ca2af80a330fc
Chukwu Matthew <codemat.biz@gmail.com>
2024-09-21 17:33:38 +0100

    updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,31 +3,31 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: process.env.HOME!,
+      url: process.env.NEXT_PUBLIC_HOME!,
       lastModified: new Date(),
     },
     {
-      url: process.env.ABOUT_US!,
+      url: process.env.NEXT_PUBLIC_ABOUT_US!,
       lastModified: new Date(),
     },
     {
-      url: process.env.ACHIEVEMENTS!,
+      url: process.env.NEXT_PUBLIC_ACHIEVEMENTS!,
       lastModified: new Date(),
     },
     {
-      url: process.env.UPCOMING_EVENTS!,
+      url: process.env.NEXT_PUBLIC_UPCOMING_EVENTS!,
       lastModified: new Date(),
     },
     {
-      url: process.env.TRAINING_WORKSHOP!,
+      url: process.env.NEXT_PUBLIC_TRAINING_WORKSHOP!,
       lastModified: new Date(),
     },
     {
-      url: process.env.GET_INVOLVED!,
+      url: process.env.NEXT_PUBLIC_GET_INVOLVED!,
       lastModified: new Date(),
     },
     {
-      url: process.env.CONTACT_US!,
+      url: process.env.NEXT_PUBLIC_CONTACT_US!,
       lastModified: new Date(),
     },
   ];
