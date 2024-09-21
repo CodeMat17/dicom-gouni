35971f8b2b3bc09f8a508484b7cf378800a69ac6
Chukwu Matthew <codemat.biz@gmail.com>
2024-09-21 22:14:07 +0100

    updated

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -15,6 +15,18 @@ const AboutUs = async () => {
     <div className='bg-gray-50 dark:bg-gray-950 w-full min-h-screen '>
       <div className='py-12'>
         <HeaderTitle title='About Us' align='center' />
+
+        <div className="px-4 space-y-3 max-w-2xl mx-auto">
+          <h1 className="text-xl font-medium text-center">Empowering Students Through Competition and Excellence</h1>
+          <p>
+            We provide platforms that allow students to compete, learn and foster an environment that challenges them to improve beyond their limits as
+            they journey into spheres unknown.
+          </p>
+          <p>
+            Join us on this transformational journey, where competition sparks
+            growth and excellence is a pursuit for the rest of life.
+          </p>
+        </div>
         <AboutUsStatements />
 
         <div className='mt-20'>
