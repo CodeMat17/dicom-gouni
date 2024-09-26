6f672b8214d0ba29cc8ee404d31505e8f5b53e3c
Chukwu Matthew <codemat.biz@gmail.com>
2024-09-26 15:33:56 +0100

    updated

--- app/loading.tsx
+++ app/loading.tsx
@@ -8,7 +8,7 @@ const Loading = () => {
         priority
         width={55}
         height={55}
-        src='/gouni_logo.jpg'
+        src='/dicom_logo.webp'
         className='rounded-full'
       />
       <p>Please wait</p>
